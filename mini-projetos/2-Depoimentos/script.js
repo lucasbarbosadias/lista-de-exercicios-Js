87ad187e882683faecb5e975ba16535550adf6a7
Lucas Barbosa Dias <lucasbarbosadias@outlook.com>
2021-06-14 13:34:53 -0300

    Adicionando Exc 2

---
+++ mini-projetos/2-Depoimentos/script.js
@@ -0,0 +1,29 @@
+let cor = document.querySelector('#card')
+let txt = document.querySelector('.card__desc')
+let autor = document.querySelector('.card__autor')
+
+function btn1() {    
+    cor.style.backgroundColor = '#eb3b5a'
+    txt.innerText = "I had my concerns that due to a tight deadline this project can't be done. But Florin proved me wrong not only he delivered an outstanding work but he managed to deliver 1 day prior to the deadline. And when I asked for some revisions he made them in MINUTES. I'm looking forward to work with him again and I totally recommend him. Thanks again Florin!"
+    autor.innerText = "ARTHUR BIRNBAUM"
+}
+function btn2() {    
+    cor.style.backgroundColor = '#fa8231'
+    txt.innerText = "Second time hiring him. Finished everything in a timely manner and his work is excellent. Can't recommend him enough."
+    autor.innerText = "PHOEBE KOTLIAR"
+}
+function btn3() {    
+    cor.style.backgroundColor = '#4b7bec'
+    txt.innerText = "Alexandru Florin never fails to impress me by the quality of his work and delivering on time. When it comes to front-end, he is definitely my go to guy. Always is a pleasure to work with Alexandru even when deadlines are tight and pressure is great. It's reassuring to have a project in such good hands."
+    autor.innerText = "LOUISA P. HUARD"
+}
+function btn4() {    
+    cor.style.backgroundColor = '#a55eea'
+    txt.innerText = "Florin has been of great help on our different web projects. He is very trustworthy and serious in the work done. Keep on the good work and energy, been a pleasure to collaborate."
+    autor.innerText = "CRISTINA AURMOOGUM"
+}
+function btn5() {    
+    cor.style.backgroundColor = '#20bf6b'
+    txt.innerText = "I hired Florin Pop based on others positive experiences, and I understand why he's so highly rated. His code is very clean, he communicates well, and he likes to offer alternative solutions."
+    autor.innerText = "J. KENT PEPPER"
+}

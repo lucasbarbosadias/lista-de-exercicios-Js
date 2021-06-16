0b7733b52ea895833d8c50831f9381f51c8d2631
Lucas Barbosa Dias <lucasbarbosadias@outlook.com>
2021-06-16 10:40:48 -0300

    Add Exc 16

---
+++ logica-de-programacao/Exc16/Ex16.js
@@ -0,0 +1,7 @@
+const ageInDays = (y, m, d) => y * 365 + m * 30 + d
+
+let years  = 21 
+let months = 6
+let days   = 16
+
+console.log(ageInDays(years, months, days))
---
+++ logica-de-programacao/Exc16/README.md
@@ -0,0 +1,3 @@
+# Descrição do Exercício:
+
+16. Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e mostre-a expressa apenas em dias.

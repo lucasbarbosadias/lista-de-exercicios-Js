6728b423426ea2ad93c1771a81162681d9299c1a
Lucas Barbosa Dias <lucasbarbosadias@outlook.com>
2021-06-16 00:15:26 -0300

    Bugfix: listagem de usuarios, filtragem e style

--- mini-projetos/4-Lista/script.js
+++ mini-projetos/4-Lista/script.js
@@ -1,37 +1,78 @@
-let listUsers = [
+const LIST_USERS = [
     {
-        nome: 'Piotr Bürkle',
-        endereco: 'Oraniembaum - Wörlitz, Germany',
+        name: 'Piotr Bürkle',
+        address: 'Oraniembaum - Wörlitz, Germany',
         image: 'https://picsum.photos/80'
     },
     {
-        nome: 'Amber Fowler',
-        endereco: 'Wagga, Australia',
+        name: 'Amber Fowler',
+        address: 'Wagga, Australia',
         image: 'https://picsum.photos/80'
     },
     {
-        nome: 'Lidia Pastor',
-        endereco: 'Oraniembaum - Wörlitz, Germany',
+        name: 'Pedro Souza',
+        address: 'São Paulo, Brasil',
         image: 'https://picsum.photos/80'
     },
     {
-        nome: 'Barry Black',
-        endereco: 'El Monte, United States',
+        name: 'Bryan James',
+        address: 'New York, United States',
+        image: 'https://picsum.photos/80'
+    },
+    {
+        name: 'Lidia Pastor',
+        address: 'Oraniembaum - Wörlitz, Germany',
+        image: 'https://picsum.photos/80'
+    },
+    {
+        name: 'Barry Black',
+        address: 'El Monte, United States',
         image: 'https://picsum.photos/80'
     }
 ]
 
-let user = document.querySelector('.list__users')
-let allUsers = ''
-listUsers.forEach(user => {
-    allUsers += `
-        <img src="${users.image}" alt="user list" class="list__img">
-        <div class="list__text">
-            <h2 class="list__name">${users.nome}</h2>
-            <p class="list__address">${users.endereco}</p>
-        </div>
-        <hr>
-    `
-})  
-user.innerHTML = allUsers
+let elementUser = document.querySelector('.list__users')
+
+const factoryUsers = (list) => {
+  const userElements = list.map(user =>`
+    <div class="list__junc">
+      <img src="${user.image}" alt="user list" class="list__img">
+      <div class="list__text">
+          <h2 class="list__name">${user.name}</h2>
+          <p class="list__address">${user.address}</p>
+      </div>
+    </div>
+  `)  
+  elementUser.innerHTML = userElements
+}
+
+const resetElementsUser = () => {
+  elementUser.innerHTML = ''
+}
+
+const filter = (value) => {
+  const newList = LIST_USERS.filter(user => user.name.includes(value))
+
+  factoryUsers(newList)
+}
+
+const handleFilterUsers = ({ target }) => {
+  const { value } = target
+  resetElementsUser()
+  filter(value)
+}
+
+document.querySelector('#filter').addEventListener('change', (event) => {
+  handleFilterUsers(event)
+})
+
+const init = () => {
+  factoryUsers(LIST_USERS)
+}
+
+init()
+
+
+
+
     
